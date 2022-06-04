ff5ccd78bb3f6ba14b8cdf2a077e17f034f18f09
Ifrat Binte <ifratbinte@gmail.com>
2022-06-04 22:02:34 +0600

    update menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,36 @@
+import {FaTh, FaUserAlt, FaRegChartBar,FaCommentAlt,FaShoppingBag,FaThList} from "react-icons/fa";
+
+const Menu = [
+    {
+        path:"/",
+        name:"Dashboard",
+        icon:<FaTh/>
+    },
+    {
+        path:"/about",
+        name:"About",
+        icon:<FaUserAlt/>
+    },
+    {
+        path:"/analytics",
+        name:"Analytics",
+        icon:<FaRegChartBar/>
+    },
+    {
+        path:"/comment",
+        name:"Comment",
+        icon:<FaCommentAlt/>
+    },
+    {
+        path:"/product",
+        name:"Product",
+        icon:<FaShoppingBag/>
+    },
+    {
+        path:"/productList",
+        name:"Product List",
+        icon:<FaThList/>
+    }
+]
+
+export default Menu;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,68 +1,31 @@
 import React, { useState } from 'react';
-import {
-    FaTh,
-    FaBars,
-    FaUserAlt,
-    FaRegChartBar,
-    FaCommentAlt,
-    FaShoppingBag,
-    FaThList
-}from "react-icons/fa";
+import {FaBars} from "react-icons/fa";
+import Menu from './Menu'
 import { NavLink } from 'react-router-dom';
 
 
 const Sidebar = ({children}) => {
     const[isOpen ,setIsOpen] = useState(false);
     const toggle = () => setIsOpen (!isOpen);
-    const menuItem=[
-        {
-            path:"/",
-            name:"Dashboard",
-            icon:<FaTh/>
-        },
-        {
-            path:"/about",
-            name:"About",
-            icon:<FaUserAlt/>
-        },
-        {
-            path:"/analytics",
-            name:"Analytics",
-            icon:<FaRegChartBar/>
-        },
-        {
-            path:"/comment",
-            name:"Comment",
-            icon:<FaCommentAlt/>
-        },
-        {
-            path:"/product",
-            name:"Product",
-            icon:<FaShoppingBag/>
-        },
-        {
-            path:"/productList",
-            name:"Product List",
-            icon:<FaThList/>
-        }
-    ]
+    
     return (
         <div className="container">
            <div style={{width: isOpen ? "50px" : "200px"}} className="sidebar">
                <div className="top_section">
                    <h1 style={{display: isOpen ? "none" : "block"}} className="logo">Logo</h1>
                    <div style={{marginLeft: isOpen ? "-5px" : "50px"}} className="bars">
-                       {/* <FaBars onClick={toggle}/> */}
                        <h2 style={{display: isOpen ? "block" : "none"}}>M</h2>
                    </div>
                </div>
                {
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeClassName="active">
-                           <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "none" : "block"}} className="link_text">{item.name}</div>
-                       </NavLink>
-                   ))
+                   Menu.map((item, index)=> {
+                       return (
+                        <NavLink to={item.path} key={index} className="link" activeClassName="active">
+                            <div className="icon">{item.icon}</div>
+                            <div style={{display: isOpen ? "none" : "block"}} className="link_text">{item.name}</div>
+                        </NavLink>
+                       )
+                   })
                }
            </div>
            <main><FaBars onClick={toggle}/>{children}</main>
